d9a8d9bb8b64bd30954100085b2a4596e92a733c
Gabriel Caldeira <69369861+GabrielCaldeiraBicalho@users.noreply.github.com>
2022-10-28 01:42:05 +0000

    versão pré-final

--- intruso.cpp
+++ intruso.cpp
@@ -19,20 +19,36 @@ void Intruso::set_senha_vazada(std::string vazou){
     codigos.push_back(cod);
 }
 std::string Intruso::crack_senha(){
+    std::string frase;
     for(std::string x : codigos){
         for(int s=0;s<6;s++){
-         if(x[s]=='A'){
-             p[s].insert({x[0],p[s][x[0]]+=1});p[s].insert({x[6],p[s][x[6]]+=1});
+         if(senhas[x][s]=='A'){
+             p[s].insert({x[0],p[s][x[0]]+=1});p[s].insert({x[1],p[s][x[1]]+=1});
+         }
+         else if(senhas[x][s]=='B'){
+             p[s].insert({x[2],p[s][x[2]]+=2});p[s].insert({x[3],p[s][x[3]]+=1});
+         }
+         else if(senhas[x][s]=='C'){
+             p[s].insert({x[4],p[s][x[4]]+=1});p[s].insert({x[5],p[s][x[5]]+=1});
+         }
+         else if(senhas[x][s]=='D'){
+             p[s].insert({x[6],p[s][x[6]]+=1});p[s].insert({x[7],p[s][x[7]]+=1});
+         }
+         else if(senhas[x][s]=='E'){
+             p[s].insert({x[8],p[s][x[8]]+=1});p[s].insert({x[9],p[s][x[9]]+=1});
          }
         }
     }
     char c_maior;
     int n_maior=0;
-    for (std::map<char,int>::iterator it=p[0].begin(); it!=p[0].end(); ++it){
+    for(int mn=0;mn<6;mn++){
+       for (std::map<char,int>::iterator it=p[mn].begin(); it!=p[mn].end(); ++it){
            if(it->second>n_maior){
                c_maior=it->first;
                n_maior=it->second;
            }
-    } std::cout<<c_maior<< " ";n_maior=0;
-    return "terminou";
+    } frase.push_back(c_maior);frase.push_back(' ');n_maior=0;
+    }
+    
+    return frase;
 }
